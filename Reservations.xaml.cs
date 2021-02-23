91d2fbff424c67ce09ed302b8dee00083612dbe6
sshaugen <s.haugen@hotmail.com>
2021-02-23 16:28:07 +0100

    Display reservationlist

--- Reservations.xaml.cs
+++ Reservations.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using HotelLibrary;
 using System.Linq;
+using Newtonsoft.Json;
 
 
 namespace FrontDeskHotel
@@ -25,8 +26,8 @@ namespace FrontDeskHotel
         static readonly HttpClientImpl clientImpl = new HttpClientImpl(client);
 
         public string FixedUri = "http://localhost:5000";
-        List<Room> rooms { get; set; }
-        List<Reservation> reservations { get; set; }
+
+        public List<Reservation> reservations { get; set; } 
 
 
         public Reservations()
@@ -36,25 +37,54 @@ namespace FrontDeskHotel
 
         private void addResBtn_Click(object sender, RoutedEventArgs e)
         {
+            resPanel.Children.Clear();
             resFrame.Content = null;
             resFrame.Content = new AddReservation();
         }
 
         private void deleteResBtn_Click(object sender, RoutedEventArgs e)
         {
+            resPanel.Children.Clear();
             resFrame.Content = null;
             resFrame.Content = new DeleteReservation();
         }
 
-        private void Reservation_OnLoaded(object sender, RoutedEventArgs e)
+        async private void Reservation_OnLoaded(object sender, RoutedEventArgs e)
         {
-            var response = clientImpl.Get(FixedUri + "/rooms?status=AVAILABLE");
+            var response = await clientImpl.Get(FixedUri + "/reservations");
+            if (!response.IsSuccessStatusCode) throw new Exception(response.StatusCode.ToString());
+            var content = response.Content.ReadAsStringAsync().Result;
+            reservations = JsonConvert.DeserializeObject<List<Reservation>>(content);
 
-            resFrame.Content = "LISTE med reservasjoner vises her";
-            
+            Label header = new Label
+            {
+                Content = "Reservation List: \n",
+                FontFamily = new FontFamily("Arial Black"),
+                FontSize = 15
+            };
+            resPanel.Children.Add(header);
+            Label resList;
+            foreach (var res in reservations)
+            {
+                resList = new Label
+                {
+                    Content = string.Join(Environment.NewLine,
+                                "ReservationID: " + res.ReservationId.ToString() +
+                                "\nCustomer: "  + res.Customer.CustomerName + 
+                                "\nRoomNumber: " + res.Room.RoomNumber.ToString() + 
+                                "\nFrom: " + res.StartDate.ToShortDateString() + 
+                                "\nTo: " + res.EndDate.ToShortDateString()
+                                 ),
+                    FontFamily = new FontFamily("Arial Black"),
+                    FontSize = 12,
+                };
 
-            
-            
+                resPanel.Children.Add(resList);
+            }
+
         }
+
+        
     }
+
 }
