42a49a6b587db75a0501af3916ef07e4e884246f
sshaugen <s.haugen@hotmail.com>
2021-02-26 21:44:29 +0100

    change room status

--- Rooms.xaml.cs
+++ Rooms.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Shapes;
 using HotelLibrary;
 using System.Net.Http;
 using Newtonsoft.Json;
+using FrontDeskHotel;
 namespace FrontDeskHotel
 {
     using HttpClientImpl;
@@ -25,18 +26,20 @@ namespace FrontDeskHotel
         public string FixedUri = "http://localhost:5000";
 
         List<Room> _rooms { get; set; }
+        string StatusContainer { get; set; }
 
         public Rooms()
         {
+            StatusContainer = null;
             InitializeComponent();
-           
+            this.DataContext = this;
         }
 
         async public void Room_OnLoaded (Object sender, RoutedEventArgs e )
         {
             var response = await clientImpl.Get(FixedUri + "/rooms");
             var content = response.Content.ReadAsStringAsync().Result;
-            var rooms = JsonConvert.DeserializeObject<List < Room >> (content);
+            var rooms = JsonConvert.DeserializeObject<List <Room>> (content);
 
             Label roomlist;
             ComboBox changeStatus;
@@ -48,36 +51,43 @@ namespace FrontDeskHotel
                     
                     Content = String.Join(Environment.NewLine,
                                           "Room: " + room.RoomNumber.ToString() + 
-                                          "\nStatus: " + room.RoomStatus.ToString()),
+                                          "\nStatus: " + room.RoomStatus.ToString() + 
+                                          "\nNote: " + room.Note),
                     FontFamily = new FontFamily("Arial Black"),
                     FontSize = 12,
                     BorderThickness = new Thickness (1,1,1,1),
                     BorderBrush = Brushes.Black,
                     Background = Brushes.LightGray,
-                    MaxWidth = 200,
-                    Width = 200,
+                    Width = 350,
                     HorizontalAlignment = HorizontalAlignment.Left
                 };
 
-                
+
                 changeStatus = new ComboBox
                 {
-                   ItemsSource = new List<string> { "AVAILABLE", "NOE", "CHRISTIAN" },
-                   FontFamily = new FontFamily("Arial Black"),
-                   SelectedIndex = 0,
-                   FontSize = 12,
-                   Width = 200,
-                   HorizontalAlignment = HorizontalAlignment.Left,
-                   Background = Brushes.White
 
-
+                    ItemsSource = new List<string> {"AVAILABLE", "CLEANING", "MAINTENANCE", "SERVICE", "BUSY" },
+                    FontFamily = new FontFamily("Arial Black"),
+                    FontSize = 12,
+                    Width = 350,
+                    HorizontalAlignment = HorizontalAlignment.Left,
+                    Background = Brushes.White,
+                    
                 };
 
-                changeStatus.SelectionChanged += (sender, e) =>
+                changeStatus.SelectionChanged += async (sender, e) =>
                 {
-                    MessageBox.Show("Status is changed");
+                    ClearPanelChildren();
+                    if (sender is ComboBox dropdown) StatusContainer = dropdown.SelectedItem as string;
+                    MessageBoxResult msg = MessageBox.Show("Change status to " + StatusContainer + " on room " + room.RoomNumber + "?", "Change Status", System.Windows.MessageBoxButton.OKCancel);
+
+                    if (msg == MessageBoxResult.OK)
+                    {
+                       var response = await clientImpl.Put(FixedUri + "/rooms/" + $"{room.RoomNumber}?newStatus={StatusContainer}");
+                    }
+                    Room_OnLoaded(this, e);
                 };
-                
+
                 Label space = new Label();
 
                 roomPanel.Children.Add(roomlist);
@@ -86,8 +96,15 @@ namespace FrontDeskHotel
             }
         }
 
-        private void SearchBtn_Click(object sender, RoutedEventArgs e)
+        private void ClearPanelChildren()
         {
+            for (int size = roomPanel.Children.Count - 1; size >= 0; size += -1)
+            {
+                UIElement elements = roomPanel.Children[size];
+                if (elements is Label || elements is ComboBox)
+                    roomPanel.Children.Remove(elements);
+
+            }
 
         }
     }
